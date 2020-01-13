0ed1d8951e2cafe7c971c60f14db6229753985d1
Filip Suster <filip.suster@t-mobile.cz>
2020-01-13 13:41:37 +0100

    Fix generate crl-ip-hostname script

--- tools/generate-crl-ip-hostname.py
+++ tools/generate-crl-ip-hostname.py
@@ -5,28 +5,25 @@ import os
 import requests
 import datetime
 
-base_url="https://raw.githubusercontent.com/threatstop/crl-ocsp-whitelist/master/"
-uri_list=['crl-hostnames.txt','crl-ipv4.txt','crl-ipv6.txt','ocsp-hostnames.txt','ocsp-ipv4.txt','ocsp-ipv6.txt']
-dict=dict()
-dict['list']=list()
-def source_read_and_add(input_file):
-	output_list=list()
-	for item in input_file:
-		item=item.rstrip()
-		output_list.append(item)
-	return output_list
+base_url = "https://raw.githubusercontent.com/threatstop/crl-ocsp-whitelist/master/"
+uri_list = ['crl-hostnames.txt', 'crl-ipv4.txt', 'crl-ipv6.txt', 'ocsp-hostnames.txt', 'ocsp-ipv4.txt', 'ocsp-ipv6.txt']
+wl = dict()
+wl['list'] = list()
 
 
 for uri in uri_list:
-	url = base_url + uri
-	r=requests.get(url)
-	dict['list'] += source_read_and_add(r.text)
+    url = base_url + uri
+    r = requests.get(url)
+    wl['list'].extend(r.text.split('\n'))
 
-dict['type'] = "string"
-dict['matching_attributes']=["hostname","domain","ip-dst","ip-src","url", "domain|ip"]
-dict['name']="CRL Warninglist"
-dict['version']= int(datetime.date.today().strftime('%Y%m%d'))
-dict['description']="CRL Warninglist from threatstop (https://github.com/threatstop/crl-ocsp-whitelist/)"
-dict['list']=list(set(dict['list']))
+wl['type'] = "string"
+wl['matching_attributes'] = ["hostname", "domain", "ip-dst", "ip-src", "url", "domain|ip"]
+wl['name'] = "CRL Warninglist"
+wl['version'] = int(datetime.date.today().strftime('%Y%m%d'))
+wl['description'] = "CRL Warninglist from threatstop (https://github.com/threatstop/crl-ocsp-whitelist/)"
+wl['list'] = list(set(wl['list']))
 
-print(json.dumps(dict))
+# Remove empty string
+wl['list'].remove('')
+
+print(json.dumps(wl))
