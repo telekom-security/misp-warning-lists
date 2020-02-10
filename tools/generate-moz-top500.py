2c5e7930f36d1e605992dbe82cfc94700ff2133f
Filip Suster <filip.suster@t-mobile.cz>
2020-02-10 18:02:45 +0100

    Fix moz-top500 generate script

--- tools/generate-moz-top500.py
+++ tools/generate-moz-top500.py
@@ -9,18 +9,14 @@ import os
 
 # TODO: Include MozRank
 
-moz_url_domains = "https://moz.com/top500/domains/csv"
-moz_url_pages = "https://moz.com/top500/pages/csv"
+moz_url_domains = "https://moz.com/top-500/download/?table=top500Domains"
 
 moz_file_domains = "/tmp/top500.domains.csv"
-moz_file_pages = "/tmp/top500.pages.csv"
 
 user_agent = {"User-agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:46.0) Gecko/20100101 Firefox/46.0"}
 
 rDomains = requests.get(moz_url_domains, headers=user_agent)
-rPages = requests.get(moz_url_pages, headers=user_agent)
 open(moz_file_domains, 'wb').write(rDomains.content)
-open(moz_file_pages, 'wb').write(rPages.content)
 
 moz_warninglist = {}
 version = int(datetime.date.today().strftime('%Y%m%d'))
@@ -46,24 +42,11 @@ with open(moz_file_domains) as csv_file:
             moz_warninglist['list'].append(v.rstrip().rstrip('/'))
             line_count += 1
 
-with open(moz_file_pages) as csv_file:
-    csv_reader = csv.reader(csv_file, delimiter=',')
-    line_count = 0
-    for row in csv_reader:
-        if line_count == 0:
-            #print(f'Column names are {", ".join(row)}')
-            line_count += 1
-        else:
-            #print(f'\t{row[0]}. {row[1]}, MozTrust: {row[5]}.')
-            v = row[1]
-            moz_warninglist['list'].append(v.rstrip().rstrip('/'))
-            line_count += 1
 
 moz_warninglist['list'] = sorted(set(moz_warninglist['list']))
 print(json.dumps(moz_warninglist))
 
 try:
     os.remove(moz_file_domains)
-    os.remove(moz_file_pages)
 except:
-    print(f'Perhaps {moz_file_domains}/{moz_file_pages} does not exist.')
+    print(f'Perhaps {moz_file_domains} does not exist.')
